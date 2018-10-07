0ae53568e18890b8c6612864b30c774bf78da5f2
Martin Pihlak <martin.pihlak@starship.co>
2018-10-07 21:20:34 +0300

    Adding connection handling

--- logentry.go
+++ logentry.go
@@ -10,17 +10,26 @@ type MongoLogEntry struct {
 	Component         string
 	Context           string
 	LogMessage        string
+	ConnectionInfo    *Connection
 	CommandParameters *PseudoJson
 	PlanInfo          *PlanSummary
+}
+
+type Connection struct {
+	ConnectionId string
+	IpAddress    string
+	Port         string
 }
 
 type LogParser struct {
 	commandParametersParser MongoLogParser
 	planSummaryParser       MongoLogParser
+
+	connectionMap map[string]*Connection
 }
 
 func NewLogParser() (parser LogParser, err error) {
-	parser.commandParametersParser, err = NewPseudoJsonParser()
+	parser.commandParametersParser, err = NewCommandParametersParser()
 	if err != nil {
 		return parser, fmt.Errorf("Cannot initialize commandParametersParser: %v", err)
 	}
@@ -29,7 +38,22 @@ func NewLogParser() (parser LogParser, err error) {
 		return parser, fmt.Errorf("Cannot initialize planSummaryParser: %v", err)
 	}
 
+	parser.connectionMap = make(map[string]*Connection)
+
 	return
+}
+
+func handleNewConnection(parser LogParser, entry MongoLogEntry, connParams map[string]string) {
+	conn := &Connection{
+		ConnectionId: "[conn" + connParams["id"] + "]",
+		IpAddress:    connParams["ip"],
+		Port:         connParams["port"],
+	}
+	parser.connectionMap[conn.ConnectionId] = conn
+}
+
+func handleCloseConnection(parser LogParser, entry MongoLogEntry) {
+	delete(parser.connectionMap, entry.Context)
 }
 
 // ParseLogEntry parses the MongoDb log line into MongoLogEntry structure
@@ -43,6 +67,40 @@ func ParseLogEntry(parser LogParser, logLine string) (result MongoLogEntry, err 
 	result.Component = logMatch["component"]
 	result.Context = logMatch["context"]
 	result.LogMessage = logMatch["message"]
+
+	if result.Component == "NETWORK" {
+		if result.Context == "[listener]" {
+			connParams := RegexpMatch(MongoNewConnectionRegex, result.LogMessage)
+			if connParams != nil {
+				handleNewConnection(result, connParams)
+			}
+		} else {
+			connParams := RegexpMatch(MongoConnectionMetadataRegex, result.LogMessage)
+			if connParams != nil {
+				handleConnectionMetadata(result, connParams)
+			} else {
+				connParams := RegexpMatch(MongoEndConnectionRegex, result.LogMessage)
+				if connParams != nil {
+					handleCloseConnection(result, connParams)
+				}
+			}
+
+
+				conn, ok := parser.connectionMap[result.Context]
+				if !ok {
+					conn = &Connection{
+						ConnectionId: result.Context,
+						IpAddress:    connParams["ip"],
+						Port:         connParams["port"],
+					}
+					parser.connectionMap[result.Context] = conn
+				}
+				// TODO: parse and add the metadata
+			}
+
+			// TODO: try also endconnection and clean up stale entries from the map
+		}
+	}
 
 	if result.Severity == "I" && result.Component == "COMMAND" {
 		// Parse the command parameters and execution plan
